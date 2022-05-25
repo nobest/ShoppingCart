9ea8763812f103b647885800b494a77e039d5d10
Semiu Hassan <semiuhassan86@gmail.com>
2022-05-25 15:04:19 +0100

    Create more test classes

---
+++ app/src/test/java/com/meticulous/shoppingcart/ui/main/MainViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.meticulous.shoppingcart.ui.main
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainViewModelTest
---
+++ app/src/test/java/com/meticulous/shoppingcart/utils/CartUtilTest.kt
@@ -0,0 +1,5 @@
+package com.meticulous.shoppingcart.utils
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CartUtilTest
