4c421a3f04de4dcb13cf819d5a8dbbdf93702407
Semiu Hassan <semiuhassan86@gmail.com>
2022-05-25 08:22:19 +0100

    Initial commit

---
+++ app/src/main/java/com/meticulous/shoppingcart/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.meticulous.shoppingcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meticulous.shoppingcart.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
